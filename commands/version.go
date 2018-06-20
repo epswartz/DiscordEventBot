bb57627f45acdbc7e48c034c05efd1769de43351
epswartz <eswartzen@gmail.com>
2018-06-20 12:45:00 -0400

    Better version output

--- commands/version.go
+++ commands/version.go
@@ -3,5 +3,5 @@ package commands
 // Prints current EventBot version.
 func Version() (string, error) {
 	// TODO check if args is nil
-	return "**Current EventBot version: 0.0.1 (Codename: Useless)**\nPublish Date: 05/24/2018", nil
+	return "**Version:** `0.0.1`\n**Codename:** `Useless`\n**Publish Date:** `05/24/2018`", nil
 }
