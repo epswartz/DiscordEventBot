b159640b96ae88fc6c32a8bc48b6ab18c58dcef8
epswartz <eswartzen@gmail.com>
2018-06-25 14:22:30 -0400

    SemVer2

--- commands/version.go
+++ commands/version.go
@@ -3,5 +3,5 @@ package commands
 // Prints current EventBot version.
 func Version() (string, error) {
 	// TODO check if args is nil
-	return "**Version:** `1.0.0`\n**Codename:** `Midly Useful`\n**Publish Date:** `06/25/2018`", nil
+	return "**Version:** `1.0.0-beta`\n**Codename:** `Midly Useful`\n**Publish Date:** `06/25/2018`", nil
 }
